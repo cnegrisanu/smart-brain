d86f2f1efffcbee281bfae78a247994a84a4fddb
Cristian Negrisanu <vollucris@gmail.com>
2018-07-23 17:25:10 +0200

    Added deploy to GitHub gh-pages option

---
+++ src/components/Rank/Rank.jsx
@@ -0,0 +1,17 @@
+
+import React from 'react';
+
+
+const Rank = () => {
+    return (
+        <div className='ma4 mt0'>
+            <div className='white f3'>
+                {'Cristian, your curent rank is ...'}
+            </div>
+            <div className='white f3'>
+                {'#5'}
+            </div>
+        </div>
+    );
+}
+export default Rank;
