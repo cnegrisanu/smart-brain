4002057db274c893cb028fc73a08ba83f724920a
Cristian Negrisanu <vollucris@gmail.com>
2018-07-24 10:44:57 +0200

    Rank Added

---
+++ src/components/Rank/Rank.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+const Rank = () => {
+    return (
+        <div>
+       <div className='white f3'>
+            {'Cristian your current Rank is...'}
+       </div>
+       <div className='white f1'>
+       {'#7'}
+       </div>
+  </div>
+    );
+}
+export default Rank;
