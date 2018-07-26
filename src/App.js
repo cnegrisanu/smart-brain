b90bc98a629846e7ef0236f16b32c176f2d25b4f
Cristian Negrisanu <vollucris@gmail.com>
2018-07-26 11:27:40 +0200

    Fixing the clarifai function to return correctly

--- src/App.js
+++ src/App.js
@@ -5,6 +5,11 @@ import Logo from './components/Logo/Logo';
 import ImageLinkForm from './components/ImageLinkForm/ImageLinkForm';
 import Rank from './components/Rank/Rank';
 import Particles from 'react-particles-js';
+import Clarifai from 'clarifai';
+
+const app = new Clarifai.App({
+  apiKey: '681195e7b95b49638874121ead95f132'
+ });
 
 const particlesOptions = {
   particles: {
@@ -29,6 +34,15 @@ class App extends Component {
 
 onButtonSubmit = () => {
   console.log('click');
+  app.models.predict("a403429f2ddf4b49b307e318f00e528b", "https://samples.clarifai.com/face-det.jpg").then(
+    function(response) {
+      // do something with response
+      console.log(response);
+    },
+    function(err) {
+      // there was an error
+    }
+  );
 }
 
   onInputChange = (event) => {
