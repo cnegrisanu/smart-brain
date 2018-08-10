230198ded447b4e508687d985090c19e618631ae
Cristian Negrisanu <vollucris@gmail.com>
2018-08-10 13:20:14 +0200

    Fixed issue with submit endpoint

--- src/components/Signin/Signin.jsx
+++ src/components/Signin/Signin.jsx
@@ -18,7 +18,7 @@ class Signin extends React.Component {
     }
 
     onSubmitSignIn = () => {
-        fetch('http://localhost:3000/signin', {
+        fetch('https://frozen-depths-68731.herokuapp.com/signin', {
             method: "POST",
             headers: {'content-Type': 'application/json'},
             body: JSON.stringify({
